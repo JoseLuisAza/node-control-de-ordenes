71be3ea8b624c75b8800a3963d08eccc8a2e6e13
wicho21 <joseluisaza@hotmail.com>
2020-11-29 23:47:00 -0600

    Respuestas

--- control-de-ordenes.js
+++ control-de-ordenes.js
@@ -46,3 +46,168 @@ app.use(function (req, res, next)
   app.listen(port, () => {
     console.log(`control-de-ordenes app escuchando en http://localhost:${port}`)
   });
+
+  app.post('/addUser', (req, res) => {
+    console.log('addUser');
+      connection.query('INSERT INTO vendedor(idvendedor) VALUES("'+req.fields.user_id+'")', function (error, results, fields) {
+        if (error) throw error;
+        console.log('Usuario registrado!');
+        res.send('Usuario registrado!');
+      });
+    })
+  
+  
+    /*Metodo para saber si un usuario ya esta registrado*/ 
+    app.post('/isUserRegistered', (req, res) => {
+      console.log('isUserRegistered');
+      /*Hacemos la consulta a la base de datos*/ 
+      connection.query('SELECT idvendedor FROM vendedor WHERE idvendedor="'+req.fields.user_id+'"', function (error, results, fields) {
+        if (error)
+        {
+           throw error;
+        }   
+        if(results.length>=1)
+        {
+          console.log("true");
+          res.send('true');
+        }
+        else{
+          console.log("false");
+          res.send('false');
+        }
+      });
+    })
+
+  app.post('/getArticulos', (req, res) => {
+    console.log('/getArticulos');
+    connection.query('SELECT idproducto,nombre,precio,ruta,detalle,fecha_creado FROM producto WHERE vendedor_idvendedor="'+req.fields.user_id+'"', function (error, results, fields) {
+        if (error)
+        {
+          throw error;
+        }      
+        console.log(results.length);
+    setTimeout(() => {
+      res.status(200).send(JSON.stringify(results));
+    }, 2000);     
+
+  });
+});
+
+app.post('/getArticulosGeneral', (req, res) => {
+  console.log('/getArticulos');
+  connection.query('SELECT idproducto,nombre,precio,ruta,detalle,fecha_creado FROM producto WHERE vendedor_idvendedor !="'+req.fields.user_id+'"', function (error, results, fields) {
+      if (error)
+      {
+        throw error;
+      }      
+      console.log(results.length);
+  setTimeout(() => {
+    res.status(200).send(JSON.stringify(results));
+  }, 2000);     
+
+});
+});
+
+app.get('/getArticulosStore', (req, res) => {
+  console.log('/getArticulos');
+  connection.query('SELECT idproducto,nombre,precio,ruta,detalle,fecha_creado FROM producto', function (error, results, fields) {
+      if (error)
+      {
+        throw error;
+      }      
+      console.log(results.length);
+  setTimeout(() => {
+    res.status(200).send(JSON.stringify(results));
+  }, 2000);     
+
+});
+});
+
+  
+  app.post('/newItem', (req, res) => {
+    console.log('newItem');
+    console.log(req.files); // contains files
+    console.log(req.fields.user_id);
+    let user_id=req.fields.user_id;//obtenemos el campo user_id de los campos a travez del objeto req
+    const nameDirectorio=user_id.replace('|', '-');//reemplazar del user_id los campos | por -
+    let pathDirectorio='assets/item/'+nameDirectorio;//establece el directorio en donde se va a guardar los libros
+    const {name, path} = req.files.file//sustraemos el nombre y el path temporal del archivo
+    let file=pathDirectorio+'/'+name;//nombre y directorio del archivo
+    let date=moment().format('YYYY-MM-DD hh:mm:ss');//obtenemos la fecha y hora
+    try //try catch para manejar los errores
+    {
+      if (!fs.existsSync(pathDirectorio))//verificamos si el directorio no existe
+      {
+          //Creamos el directorio
+          let dir=fs.mkdirSync(pathDirectorio,{ recursive: true }, (err) => {
+            console.error(err);
+            console.error("Hubo un herror al crear directorio!");
+          })
+          console.log('directorio '+dir+" creado!");//mostramos mensaje en consola si no hubo errores
+          fs.writeFileSync(file, fs.readFileSync(path));//Escribimos al directorio nuevo del directorio viejo
+          connection.query('INSERT INTO producto(nombre,precio,ruta,detalle,fecha_creado,vendedor_idvendedor) VALUES("'+req.fields.nombre+'","'+req.fields.precio+'","'+file+'","'+req.fields.detalles+'","'+date+'","'+user_id+'")', function (error, results, fields) {
+            if (error) throw error;
+            console.info('Archivo subido! ');//si no hubo ningun problema con el copiado
+            res.status(201).send('Libro subido');//respondemos con codigo de status ok
+          });
+        }
+      else{
+        //si el directorio si existe
+        try {
+          fs.readFileSync(file, 'utf8');//Verificamos si el archivo ya existe
+          console.error('Archivo ya existe');//Mostramos en consola que el archivo ya existe
+          res.status(200).send('Libro ya existe');//respondemos con codigo de status ok
+        } catch (err) {
+          console.error(err)//mostramos el error en consola
+          fs.writeFileSync(file, fs.readFileSync(path));//Escribimos al directorio nuevo del directorio viejo
+          connection.query('INSERT INTO producto(nombre,precio,ruta,detalle,fecha_creado,vendedor_idvendedor) VALUES("'+req.fields.nombre+'","'+req.fields.precio+'","'+file+'","'+req.fields.detalles+'","'+date+'","'+user_id+'")', function (error, results, fields) {
+            if (error) throw error;
+            console.log(results);
+            if(results.affectedRows>=1)
+            {
+              connection.commit((err) =>{
+                if (err) { 
+                  connection.rollback(()=> {
+                    throw err;
+                  });
+                }
+                console.info('Archivo subido! ');//si no hubo ningun problema con el copiado
+                res.status(201).send('Articulo subido');//respondemos con codigo de status ok
+              });
+            }
+          });
+         
+        }
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).send(err);
+    }
+  });
+
+
+//Se debe implementa COMMIT Y ROLLBACK
+app.post('/deleteItem',  (req, res)=> {
+  console.log('deleteItem');
+  console.log(req.fields);
+  connection.query('DELETE FROM producto WHERE idproducto='+req.fields.iditem+' AND vendedor_idvendedor="'+req.fields.user_id+'"', function (error, results, fields) {
+    if (error) throw error;
+    console.log(results.affectedRows);
+    //Eliminamos el fichero si y solo si fue eliminado de la base de datos
+    if(results.affectedRows>=1)
+    {
+        try //try catch para manejar los errores
+        {
+          fs.unlinkSync(req.fields.path.toString());
+          res.status(200).send(JSON.stringify(results));
+        }catch(err)
+        {
+          console.error(err);
+          res.status(500).send(err);  
+        }
+    }
+    else{
+      res.status(200).send(JSON.stringify(results));
+    }
+  });
+});
